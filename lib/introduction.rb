db57230f11d6c41c116f3ddb1c37c928a400f0df
charlessullivan <sullivanforfamily@gmail.com>
2021-01-16 00:24:35 -0800

    commit tree ec4aaf9897f81afcdc149c4bc7056143af5522cb

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+def say_something(name)
+    puts "Hi #{name}!"
+end
+
+say_something("")
+
+def say_something_x(name,language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+
+say_something_x( , )
